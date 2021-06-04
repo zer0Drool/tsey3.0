4b7fd7636d65e8becbb07b46142069d6c38469b5
AVD <avd@AVDs-Mac-mini.local>
2021-06-04 15:34:55 +0100

    loool

--- public/script.js
+++ public/script.js
@@ -9,54 +9,71 @@
     // document title
     let docTitleCount = 0;
     let docTitle = [
-        "/",
-        "//",
-        "///",
-        "////",
-        "/////",
-        "//////",
-        "///////",
-        "////////",
-        "/////////",
-        "//////////",
-        "///////////",
-        "////////////",
+        // "/",
+        // "//",
+        // "///",
+        // "////",
+        // "/////",
+        // "//////",
+        // "///////",
+        // "////////",
+        // "/////////",
+        // "//////////",
+        // "///////////",
+        // "////////////",
+        'g',
+        'gi',
+        'giv',
+        'give',
+        'give m',
+        'give me',
+        'give me a',
+        'give me a j',
+        'give me a jo',
+        'give me a job',
+        'give me a job p',
+        'give me a job pl',
+        'give me a job plz',
+        '* give me a job plz *',
+        'give me a job plz',
+        '* give me a job plz *',
+        'give me a job plz',
+        '* give me a job plz *',
     ];
-    // ip
-    // let zerocool = {
-    //     device: null,
-    //     ip: null,
-    //     ip2: null,
-    //     referrer: null,
-    //     country: null,
-    //     city: null,
-    // };
-    // let info = document.getElementById("info");
-    // let deetsInt;
-    // let snoopDeets;
-    // let snoopDeetsCount = 0;
-    // imgs
-    // let backgroundImg = document.getElementById("background-img");
-    // let perspective = document.getElementById("perspective");
     // three.js
     let scene, camera, form, cameraInt;
-    // let currentPos = 0;
-    // let cameraPositions = [
-    //     [0, 0, 6],
-    //     [10, 14, 4],
-    //     [-5, 8, 1],
-    // ];
     // nav
     let checkboxes = document.getElementsByTagName('input');
-    let navLinks = document.getElementsByClassName('nav-link');
+    let labels = document.getElementsByTagName('label');
     let nav = document.getElementById('nav');
     let checked = [];
+    let navLinks = document.getElementsByClassName('nav-link');
+
     //work
     let work = document.getElementById('work');
 
+    let background = document.getElementById('background-grad');
+    background.style.background = `radial-gradient(ellipse at 10% 10%, rgba(${rando(255, 0)}, ${rando(255, 0)}, ${rando(255, 0)}, ${rando(1, 0)}), transparent), radial-gradient(ellipse at 80% 90%, rgba(${rando(255, 0)}, ${rando(255, 0)}, ${rando(255, 0)}, ${rando(1, 0)}), rgba(${rando(255, 0)}, ${rando(255, 0)}, ${rando(255, 0)}, ${rando(1, 0)}))`;
+
     // FUNCTIONS
+    function promiseTimeout(timeout) {
+        return new Promise((resolve, reject) => setTimeout(resolve, timeout));
+    };
+
     function rando(max, min) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    function randoLet() {
+        var letters = ["a", "b", "c", "d", "e", "f"];
+        var letter = letters[Math.floor(Math.random() * letters.length)];
+        return letter
+    }
+
+    function randoHex() {
+        let hex =  "000000".replace(/0/g,function(){return (~~(Math.random()*16));});
+        console.log(parseInt(hex));
+        return parseInt(hex);
     };
 
     function titlechange() {
@@ -97,7 +114,7 @@
         .start();
     };
 
-    cameraInt = setInterval(moveCamera, rando(10000, 3000));
+    // cameraInt = setInterval(moveCamera, rando(10000, 3000));
 
     // THREE.JS
     function threeInit() {
@@ -123,25 +140,21 @@
 
         // form 1
         var geometry = new THREE.SphereGeometry(1, 128, 128);
-        // var material = new THREE.MeshNormalMaterial();
+        let col = randoHex();
         var material = new THREE.MeshPhongMaterial({
-          //   color: 0xff1493,
-          //   emissive: 0xffffff,
-          //   emissiveIntensity: 0.1,
-          //   metalness: 0.9,
-          //   reflectivity: 1,
-          color: 0x000000,
-          specular: 0xffffff,
-          emissive: 0x333333,
-          shininess: 10,
+            color: col,
+            specular: randoHex(),
+            emissive: randoHex(),
+            emissiveIntensity: 0.5,
+            shininess: 20,
         });
+        console.log(material);
         form = new THREE.Mesh(geometry, material);
         form.position.set(0, 0, 0);
         form.scale.set(2, 3, 1);
         scene.add(form);
 
         camera.position.set(0, 0, 6);
-        // camera.lookAt(formLocations[0][0], formLocations[0][1], formLocations[0][2]);
 
         var simplex = new SimplexNoise();
         var update = function () {
@@ -172,37 +185,46 @@
     }
 
     function check(e) {
-        console.log(e.target.name, e.target.checked);
+
+        moveCamera();
+
         if (e.target.checked) {
-            checked.push(e.target.name)
+
+            checked.push(e.target.name);
+
+            Array.from(navLinks).forEach(elem => {
+                if (elem.classList.contains(e.target.name)) {
+                    elem.style.transition = `opacity ${rando(0.6, 0.3)}s linear`;
+                    elem.style.display = 'block';
+                    setTimeout(() => {
+                        elem.style.opacity = '1';
+                    }, 10);
+                };
+            });
+
         } else {
+
             checked = checked.filter(item => item !== e.target.name);
+
+            Array.from(navLinks).forEach(elem => {
+                if (elem.classList.contains(e.target.name)) {
+                    elem.style.transition = `opacity 0.2s linear`;
+                    elem.style.opacity = '0';
+                    setTimeout(() => {
+                        elem.style.display = 'none';
+                    }, 300);
+                };
+            });
+
         };
 
-        // if (!checked.length) {
-        //     for (var i = 0; i < navLinks.length; i++) {
-        //         navLinks[i].style.display = 'block';
-        //     };
-        // } else {
-            for (var i = 0; i < navLinks.length; i++) {
-                navLinks[i].style.display = 'none';
-            };
-
-            for (var i = 0; i < navLinks.length; i++) {
-                checked.forEach((item) => {
-                    if (navLinks[i].classList.contains(item)) {
-                        navLinks[i].style.display = 'block';
-                    };
-                });
-            };
-        // };
     };
 
     function createPositions(elem, iter) {
         obj = {
             height: elem.offsetHeight,
             width: elem.offsetWidth,
-            top: rando(window.innerHeight - elem.offsetHeight - nav.offsetHeight, 10),
+            top: rando(window.innerHeight - elem.offsetHeight - nav.offsetHeight - 10, 10),
             left: rando(window.innerWidth - elem.offsetWidth, 0),
             created: iter,
         };
@@ -233,7 +255,7 @@
     };
 
     posLoop().then(() => {
-        console.log(positions);
+        console.log('positions', positions);
         for (var i = 0; i < navLinks.length; i++) {
             navLinks[i].style.top = `${positions[i].top}px`;
             navLinks[i].style.left = `${positions[i].left}px`;
@@ -241,7 +263,7 @@
     }).then(() => {
         for (var i = 0; i < navLinks.length; i++) {
             navLinks[i].style.display = 'none';
-            navLinks[i].style.opacity = '1';
+            // navLinks[i].style.opacity = '1';
         };
     });
 
